14b753a943ba16a166b6958173bf555a4ad840f3
changeAJay <ajajay008@gmail.com>
2022-03-09 11:07:00 +0530

    UI background setup

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ from tkinter import *
 
 window = Tk()
 window.title("Password Generator")
-window.config(padx=100, pady=60, bg='yellow')
+window.config(padx=100, pady=60, )
 #window.wm_attributes('-transparentcolor', GREEN)
 
-canvas = Canvas(width=200, height=223, bg='yellow', highlightthickness=0)
+canvas = Canvas(width=600, height=600, highlightthickness=0)
 t_image = PhotoImage(file='icon.png')
-canvas.create_image(100, 112, image=t_image)
-time_text = canvas.create_text(100, 130, text='00:00', font=('Ariel', 35, 'bold'), fill='white' )
+canvas.create_image(300, 112, image=t_image)
+#time_text = canvas.create_text(100, 130, text='00:00', font=('Ariel', 35, 'bold'), fill='white' )
 canvas.grid(column=1, row=1)
+
+
+window.mainloop()

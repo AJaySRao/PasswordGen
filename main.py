e659bba441e7812cee6cd80b782d2a534d508e84
changeAJay <ajajay008@gmail.com>
2022-03-12 08:32:10 +0530

    UI Setup, Labels, Entries and buttons

--- main.py
+++ main.py
@@ -3,36 +3,38 @@ from turtle import width
 
 window = Tk()
 window.title("Password Generator")
-window.config(padx=20, pady=40)
-#window.wm_attributes('-transparentcolor', GREEN)
+window.config(padx=20, pady=20)
 
-canvas = Canvas(width=256, height=256, highlightthickness=0)
+canvas = Canvas(width=256, height=256)
 t_image = PhotoImage(file='icon.png')
 canvas.create_image(123, 123, image=t_image)
-#time_text = canvas.create_text(100, 130, text='00:00', font=('Ariel', 35, 'bold'), fill='white' )
 canvas.grid(row=0, column=1)
 
-
-website = Label(text='Website:', font= ('Courier', 12, 'normal'))
+#Labels-------------------------
+website = Label(text='Website:')
 website.grid(row=1, column=0)
 
-username = Label(text='Email/Username:', font= ('Courier', 12, 'normal'))
+username = Label(text='Email/Username:')
 username.grid(row=2, column=0)
 
-password = Label(text='Password:', font= ('Courier', 12, 'normal'))
+password = Label(text='Password:')
 password.grid(row=3, column=0)
 
+#Entry fields--------------------
 website_field = Entry(width=35)
-website_field.grid(row=1, column=1, columnspan=3)
+website_field.grid(row=1, column=1, columnspan=2, sticky='EW')
 
 un_field = Entry(width=35)
-un_field.grid(row=2, column=1, columnspan=3)
+un_field.grid(row=2, column=1, columnspan=2, sticky='EW')
 
 pw_field = Entry(width=21)
-pw_field.grid(row=3, column=1)
+pw_field.grid(row=3, column=1, sticky='EW')
 
-g_pwd = Button(text='Generate Password', font= ('Courier', 10, 'normal'))
-g_pwd.grid(row=3, column=2 , columnspan=2)
+#Buttons--------------------------
+pwd_btn = Button(text='Generate Password')
+pwd_btn.grid(row=3, column=2)
 
+add_btn = Button(text='Add', width=36)
+add_btn.grid(row=4, column=1 , columnspan=2, sticky='EW')
 
 window.mainloop()
